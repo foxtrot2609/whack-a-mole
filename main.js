02e0cf0ef946eb5d5cd52ad5e82ed5775fba43c7
foxtrot2609 <73159713+foxtrot2609@users.noreply.github.com>
2021-03-17 13:04:11 +0300

    feat: add random functions to js

---
+++ main.js
@@ -0,0 +1,20 @@
+/* Variables */
+const holes = document.querySelectorAll(".hole");
+const score = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+
+/* Functions */
+const randomTime = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const randomHole = (holes) => {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+};
