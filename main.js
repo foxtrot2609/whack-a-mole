6158e64702ad5aada7a08c17a92a15e1f2a5a726
foxtrot2609 <73159713+foxtrot2609@users.noreply.github.com>
2021-03-18 12:58:11 +0300

    feat: add js code and css styles for moles and score

--- main.js
+++ main.js
@@ -2,21 +2,60 @@
 const holes = document.querySelectorAll(".hole");
 const score = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
-let lastHole;
+let lastMole;
+let timeUp = false;
+const gameTime = 10000;
+let count;
+let timerId;
 
 /* Functions */
+
+const startGame = () => {
+  count = 0;
+  score.textContent = count;
+  timeUp = true;
+  showMole();
+  setTimeout(() => {
+    timeUp = false;
+    startBtn.addEventListener("click", startGame, { once: true });
+  }, gameTime);
+};
+
 const randomTime = (min, max) => {
   return Math.round(Math.random() * (max - min) + min);
 };
 
-const randomHole = (holes) => {
+const randomMole = (moles) => {
   const index = Math.floor(Math.random() * holes.length);
-  const hole = holes[index];
-  if (hole === lastHole) {
-    console.log("the same");
-    return randomHole(holes);
+  const mole = moles[index];
+  if (mole === lastMole) {
+    return randomMole(moles);
   }
-  lastHole = hole;
-  console.log(hole);
-  return hole;
+  lastMole = mole;
+  return mole;
 };
+
+const showMole = () => {
+  const time = randomTime(1200, 1800);
+  const mole = randomMole(moles);
+  mole.classList.add("up");
+  mole.addEventListener("transitionend", () => mole.classList.add("active"), {
+    once: true,
+  });
+  timerId = setTimeout(() => {
+    mole.classList.remove("up");
+    mole.classList.remove("active");
+    if (timeUp) showMole();
+  }, time);
+};
+
+function changeScore() {
+  score.textContent = ++count;
+  clearTimeout(timerId);
+  this.classList.remove("up");
+  this.classList.remove("active");
+  if (timeUp) showMole();
+}
+
+startBtn.addEventListener("click", startGame, { once: true });
+moles.forEach((mole) => mole.addEventListener("click", changeScore));
