537d586f4a7fc36813edc898b650850a4486f4f4
foxtrot2609 <73159713+foxtrot2609@users.noreply.github.com>
2021-03-30 22:27:25 +0300

    fix: change code for best readability

--- main.js
+++ main.js
@@ -4,55 +4,20 @@
 let lastMole;
 let timeUp = false;
 let currentLevel = localStorage.getItem("level");
+let playedGames = localStorage.getItem("played");
 // constants
 let count;
 let timerId;
-let startOfTimeGap = 2000;
-let endOfTimeGap = 3000;
-const gameTime = 5000;
+let startOfTimeGap = 1500;
+let endOfTimeGap = 2000;
+const gameTime = 10000;
+const timeStep = 100;
 // elements
 const holes = document.querySelectorAll(".hole");
 const score = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 
 /* Functions */
-
-const getBestResult = () => {
-  if (localStorage.getItem("result") === null) {
-    result.textContent = "0";
-  } else {
-    result.textContent = localStorage.getItem("result");
-  }
-};
-
-const getLevel = () => {
-  if (currentLevel === null) {
-    level.textContent = "0";
-  } else {
-    level.textContent = currentLevel;
-    startOfTimeGap -= +currentLevel * 100;
-    endOfTimeGap -= +currentLevel * 100;
-  }
-};
-
-const setLevel = () => {
-  +currentLevel++;
-  localStorage.setItem("level", currentLevel);
-  level.textContent = currentLevel;
-  startOfTimeGap -= 100;
-  endOfTimeGap -= 100;
-};
-
-const startGame = () => {
-  count = 0;
-  score.textContent = count;
-  timeUp = true;
-  showMole();
-  setTimeout(() => {
-    timeUp = false;
-    startBtn.addEventListener("click", startGame, { once: true });
-  }, gameTime);
-};
 
 const randomTime = (min, max) => {
   return Math.round(Math.random() * (max - min) + min);
@@ -68,52 +33,109 @@ const randomMole = (moles) => {
   return mole;
 };
 
-function checkLevel() {
-  if (timeUp) {
-    showMole();
+const getBestResult = () => {
+  if (localStorage.getItem("result") === null) {
+    result.textContent = "0";
   } else {
-    this.addEventListener(
-      "transitionend",
-      () => {
-        if (count >= gameTime / 1000) setLevel();
-      },
-      {
-        once: true,
-      }
-    );
+    result.textContent = localStorage.getItem("result");
   }
 };
 
-const showMole = () => {
-  const time = randomTime(startOfTimeGap, endOfTimeGap);
-  const mole = randomMole(moles);
-  mole.classList.add("up");
-  mole.addEventListener("transitionend", () => mole.classList.add("active"), {
-    once: true,
-  });
-  timerId = setTimeout(() => {
-    mole.classList.remove("up");
-    mole.classList.remove("active");
-    checkLevel();
-  }, time);
-};
-
-function changeScore() {
-  score.textContent = ++count;
-  clearTimeout(timerId);
-  this.classList.remove("up");
-  this.classList.remove("active");
-  checkLevel();
-
-  // set best result
+const setBestResult = () => {
   if (count > localStorage.getItem("result")) {
     localStorage.setItem("result", count);
     result.textContent = localStorage.getItem("result");
   }
 };
 
-moles.forEach((mole) => mole.addEventListener("click", changeScore));
+const getPlayed = () => {
+  if (playedGames === null) {
+    played.textContent = "0";
+  } else {
+    played.textContent = playedGames;
+  }
+};
+
+const setPlayed = () => {
+  +playedGames++;
+  played.textContent = playedGames;
+  localStorage.setItem("played", playedGames);
+};
+
+const getLevel = () => {
+  if (currentLevel === null) {
+    level.textContent = "0";
+  } else {
+    level.textContent = currentLevel;
+    startOfTimeGap -= +currentLevel * timeStep;
+    endOfTimeGap -= +currentLevel * timeStep;
+  }
+};
+
+const setLevel = () => {
+  +currentLevel++;
+  localStorage.setItem("level", currentLevel);
+  level.textContent = currentLevel;
+  startOfTimeGap -= timeStep;
+  endOfTimeGap -= timeStep;
+};
+
+const startGame = () => {
+  count = 0;
+  score.textContent = count;
+  timeUp = true;
+  showMole();
+  setTimeout(() => {
+    timeUp = false;
+    startBtn.addEventListener("click", startGame, { once: true });
+  }, gameTime);
+};
+
+const showMole = () => {
+  const time = randomTime(startOfTimeGap, endOfTimeGap);
+  const mole = randomMole(moles);
+  mole.classList.add("up");
+  mole.addEventListener("transitionend", 
+    () => mole.classList.add("active"), 
+    {
+      once: true,
+    }
+  );
+  timerId = setTimeout(() => {
+    mole.classList.remove("up");
+    mole.classList.remove("active");
+    setCounters();
+  }, time);
+};
+
+function clickMole() {
+  score.textContent = ++count;
+  clearTimeout(timerId);
+  this.classList.remove("up");
+  this.classList.remove("active");
+  setCounters();
+  setBestResult();
+}
+
+function setCounters() {
+  if (timeUp) {
+    showMole();
+  } else {
+    this.addEventListener("transitionend",
+      () => {
+        setPlayed();
+        if (count >= gameTime / 1000) setLevel();     
+      },
+      {
+        once: true,
+      }
+    );
+  }
+}
+
+moles.forEach((mole) => mole.addEventListener("click", clickMole));
 startBtn.addEventListener("click", startGame, { once: true });
 
 getBestResult();
 getLevel();
+getPlayed();
